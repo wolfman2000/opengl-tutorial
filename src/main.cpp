d9209ffb4dd32cd7311c9093c0394539935750cb
Jason Felds <jafelds@gmail.com>
2016-11-16 21:40:36 -0500

    Basic shaders covered.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <SDL_opengl.h>
 
 #include <memory>
+#include <iostream>
+#include <fstream>
 
 struct SDL_Deleter {
   void operator()(SDL_Window* ptr) {
@@ -18,8 +20,23 @@ struct SDL_Deleter {
   }
 };
 
+char * readShader(std::string path) {
+  std::ifstream t;
+  int length;
+  t.open(path);
+  t.seekg(0, std::ios::end);
+  length = t.tellg();
+  t.seekg(0, std::ios::beg);
+  char *buffer = new char[length];
+  t.read(buffer, length);
+  t.close();
+  return buffer;
+}
+
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
+
+  auto *base_path = SDL_GetBasePath();
 
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -37,6 +54,55 @@ int main(int argc, char *argv[]) {
   glewExperimental = GL_TRUE;
   glewInit();
 
+  // set up the vertices. Each vertex is in (X, Y) format.
+  float vertices[] = {
+    0.f, 0.5f,
+    0.5f, -0.5f,
+    -0.5f, -0.5f
+  };
+
+  // Set up the vertex array object for storing vbo references.
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // The vertices must be loaded into a vertex buffer object.
+  // Load it here.
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+
+  // make the VBO active.
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+  // copy the vertex data over.
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  auto *vertexSource = readShader(std::string() + base_path + "vertex-shader.glsl");
+  glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+  glCompileShader(vertexShader);
+
+  auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  auto *fragmentSource = readShader(std::string() + base_path + "fragment-shader.glsl");
+  glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
+  glCompileShader(fragmentShader);
+
+  // create a shader program and join the two.
+  auto shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+
+  // In case we end up with multiple buffers, good to set up.
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  // set up the position.
+  auto posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
   SDL_Event windowEvent;
 
   for (;;) {
@@ -52,10 +118,14 @@ int main(int argc, char *argv[]) {
     glClearColor(0.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     SDL_GL_SwapWindow(window->get());
   }
 
   SDL_GL_DeleteContext(context);
+
+  SDL_free(base_path);
 
   SDL_Quit();
 
